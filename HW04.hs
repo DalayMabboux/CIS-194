13df72c75e4c98c4e74283ca1040b9b750d1da4e
Dalay Mabboux <dalay.mabboux@gmail.com>
2016-02-22 21:23:12 +0100

    Finished HW05.hs

--- HW04.hs
+++ HW04.hs
@@ -15,7 +15,7 @@ x = P [1]
 -- instance (Num a, Eq a) => Eq (Poly a) where
 --     (P a) == (P b) = r a == r b
 --        where r z = dropWhile (==0) z
- 
+
 -- Exercise 3 -----------------------------------------
 -- instance (Num a, Eq a, Show a) => Show (Poly a) where
 --     show (P a) = show a
@@ -25,28 +25,33 @@ instance (Num a, Eq a, Show a) => Show (Poly a) where
            s [] = []
            s ((i,z):zs)
              | z == 0 = s zs
-             | i == 0 = [(sz z)] ++ s zs
-             | i == 1 = [(sz z ++ "x")] ++ s zs
-             | otherwise = [(sz z ++ "x^" ++ show i)] ++ s zs
+             | i == 0 = sz z : s zs
+             | i == 1 = (sz z ++ "x") : s zs
+             | otherwise = (sz z ++ "x^" ++ show i) : s zs
            sz 0 = ""
            sz (-1) = "-"
-           sz y = (show y)
+           sz y = show y
 
 -- Exercise 4 -----------------------------------------
 -- postfixZero :: Num a => Int -> [a] -> [a]
 -- postfixZero c l = l ++ replicate c 0
 
 plus :: Num a => Poly a -> Poly a -> Poly a
-plus (P a) (P b) = P $ map (\(k,l)->k+l) $ zip (f a b) (f b a)
+plus (P a) (P b) = P $ zipWith (+) (f a b) (f b a)
   where f m n
           | length m < length n = postfixZero (length n - length m) m
           | otherwise = m
         postfixZero c l = l ++ replicate c 0
 
 -- Exercise 5 -----------------------------------------
+mulShift :: Num a => Int -> [a] -> [a] -> [[a]]
+mulShift _ [] _ = []
+mulShift s (a:as) p2s = (replicate s 0 ++ map (*a) p2s) : mulShift (s+1) as p2s
 
 times :: Num a => Poly a -> Poly a -> Poly a
-times = undefined
+times (P a) (P b) = sumPolyList polyList
+          where polyList = mulShift 0 a b
+                sumPolyList (c:ps) = foldr (\n accu -> accu `plus` P n) (P c) ps
 
 -- Exercise 6 -----------------------------------------
 
@@ -75,4 +80,3 @@ class Num a => Differentiable a where
 
 instance Num a => Differentiable (Poly a) where
     deriv = undefined
-
